ee6dcd7d63d490663eec5dc44df28d8e850ed872
a-d-spina-student <a.d.spina@student.rug.nl>
2017-10-03 14:58:42 +0200

    added new callback for the heroku website on the logins

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
   facebook: {
     clientID: '117406538827502',
     clientSecret: 'dbe9aa0aff3594fd58d1b90e63c6bf29',
-    callbackURL: 'https://whattheworld.herokuapp.com/auth/facebook/callback'
+    callbackURL: 'https://whattheworld.herokuapp.com//auth/facebook/callback'
 	},
   twitter: {
     consumerKey: '6pSCk0LiGyRABQeiBkz6QQWsM',
@@ -12,19 +12,17 @@ var ids = {
   google: {
     clientID: '303458968862-d2aa9cn7c424njtv826bjo8kg43dnljr.apps.googleusercontent.com',
     clientSecret: 'VyYlcVk4nkP2Cueu2z2EjnCM',
-    callbackURL: "https://whattheworld.herokuapp.com/auth/twitter/callback"
-    callbackURL: 'http://127.0.0.1:3000/auth/google/callback'
+    callbackURL: 'https://whattheworld.herokuapp.com/auth/google/callback'
   },
   instagram: {
     clientID: 'get_your_own',
     clientSecret: 'get_your_own',
-    callbackURL: "https://whattheworld.herokuapp.com/auth/twitter/callback"
-    callbackURL: 'http://127.0.0.1:3000/auth/instagram/callback'
+    callbackURL: 'https://whattheworld.herokuapp.com/auth/instagram/callback'
   },
   foursquare: {
     clientID: 'get_your_own',
     clientSecret: 'get_your_own',
-    callbackURL: 'https://whattheworld.herokuapp.com/foursquare/callback'
+    callbackURL: 'https://whattheworld.herokuapp.com/auth/foursquare/callback'
   }
 
 };
